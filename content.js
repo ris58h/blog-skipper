7380cb057755b308390430f17d136f39c8a59746
Ilya Rodionov <ris58h@gmail.com>
2018-02-06 00:44:57 +0300

    prototype

--- content.js
+++ content.js
@@ -17,7 +17,83 @@ load(function (settings) {
 	if (settings.shortcuts) {
 		shortcuts = settings.shortcuts;
 	}
+
+	if (commentSelector == null) {
+		const stats = {};
+		const commentCandiateList = document.querySelectorAll('[class*="comment"] :not(:empty)');
+		for (const commentCandidate of commentCandiateList) {
+			if (!commentCandidate.innerText || commentCandidate.innerText.trim().length == 0) {
+				continue;
+			}
+			if (commentCandidate.tagName == 'SPAN') {
+				continue;
+			}
+			let element = commentCandidate;
+			do {
+				const propName = '__blog-skipper-stats';
+				if (!element[propName]) {
+					element[propName] = true;
+					for (const className of element.classList) {
+						if (!className.includes('comment')) {
+							continue;
+						}
+						if (!stats[className]) {
+							stats[className] = 0;	
+						}
+						stats[className]++;
+					}
+				} else {
+					break;
+				}
+			} while ((element = element.parentElement) != null);
+		}
+		console.log(stats);//TODO
+		const topClasses = topStatsClasses(stats);
+		console.log(topClasses);//TODO
+		let minAvgDepth = -1;
+		let bestClass = null;
+		for (const topClassName of topClasses) {
+			let depthSum = 0;
+			const es = document.querySelectorAll('.' + topClassName);
+			for (const e of es) {
+				depthSum += depth(e);	
+			}
+			const avgDepth = depthSum / es.length;
+			if (avgDepth > minAvgDepth) {
+				minAvgDepth = avgDepth;
+				bestClass = topClassName;
+			}
+		}
+		if (bestClass != null) {
+			commentSelector = '.' + bestClass;
+		}
+	}
+	console.log('>>> commentSelector: ' + commentSelector);//TODO
 });
+
+function topStatsClasses(stats) {
+	let topCount = 0;
+	let topClasses = [];
+	for (const className of Object.keys(stats)) {
+		const count = stats[className];
+		if (count > topCount) {
+			topCount = count;
+			topClasses = [];
+			topClasses.push(className);
+		} else if (count == topCount) {
+			topClasses.push(className);
+		}
+	}
+	return topClasses;
+}
+
+function depth(element) {
+	let res = 0;
+	while ((element = element.parentElement) != null) {
+		res++;
+	}
+	return res;
+}
 
 let prescrollPosition = null;
 
