048a3b293956e52c2838d3f6ef8dc21166b95ed5
Ilya Rodionov <ris58h@gmail.com>
2018-04-23 19:13:44 +0300

    Habrahabr.test.

---
+++ test/habrahabr.test.js
@@ -0,0 +1,33 @@
+const puppeteer = require('puppeteer');
+const assert = require("assert");
+
+let page;
+let browser;
+// const width = 1200;
+// const height = 700;
+
+before(async () => {
+    browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 100,
+        // args: [`--window-size=${width},${height}`]
+    });
+    page = await browser.newPage();
+    // await page.setViewport({ width, height });
+
+    await page.goto("file://" + process.cwd() + "/test/habrahabr.html");
+    await page.addScriptTag({ path: process.cwd() + '/utils.js' });
+    await page.addScriptTag({ path: process.cwd() + '/skipper.js' });
+});
+
+describe('habrahabr.ru', () => {
+    it('should work', async () => {
+        await page.evaluate('document.querySelector("#comment_10769168").scrollIntoView()');
+        const nextTargetId = await page.evaluate('nextTarget(window.scrollY + 1, {autoDetectComments: true}).id');
+        assert.equal("comment_10769254", nextTargetId);
+    });
+});
+
+after(() => {
+    browser.close()
+});
