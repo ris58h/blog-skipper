cc64c85fd291b6d24e018bd831a4b1113b38061f
Ilya Rodionov <ris58h@gmail.com>
2018-04-24 15:13:55 +0300

    news.ycombinator test stub.

---
+++ test/news.ycombinator.test.js
@@ -0,0 +1,17 @@
+const helper = require("./helper");
+
+describe('news.ycombinator.com', () => {
+    let page;
+
+    before(async () => {
+        page = await helper.createPage(browser, "news.ycombinator.html");
+    });
+
+    it('next comment root', async () => {
+        await helper.testNextTop(page, "#unv_16909828", "#unv_16909655");
+    });
+
+    after(async () => {
+        await page.close();
+    });
+});
