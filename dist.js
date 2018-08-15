0cbdcc9c1f449a3c9f17ad3591de3929a4f91e09
Ilya Rodionov <ris58h@gmail.com>
2018-08-15 09:13:39 +0300

    Move extension files to separate directory.

--- dist.js
+++ dist.js
@@ -1,20 +1,15 @@
 const fs = require('fs-extra')
+const archiver = require('archiver')
 
 {(async function() {
     await fs.remove("./dist")
-    const manifest = await fs.readJson("./manifest.json")
-    const dest = `./dist/blog-skipper-${manifest.version}`
-    async function copyToDest(src) {
-        await fs.copy("./" + src, dest + "/" + src)
-    }
+    await fs.ensureDir("./dist")
 
-    await fs.ensureDir(dest)
-    await copyToDest("icons")
-    await copyToDest("options")
-    await copyToDest("background.js")
-    await copyToDest("content.js")
-    await copyToDest("core.js")
-    await copyToDest("manifest.json")
-    await copyToDest("settings.js")
-    await copyToDest("settings.json")
+    const version = (await fs.readJson("./extension/manifest.json")).version
+    const output = fs.createWriteStream(`./dist/blog-skipper-${version}.zip`)
+    const archive = archiver("zip")
+    archive.pipe(output)
+    archive.glob("**/*", { cwd: "./extension" })
+    archive.on("error", err => { throw err })
+    archive.finalize()
 })()}
